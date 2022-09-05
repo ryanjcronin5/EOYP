e1fa8dda2e75e2da7aedf8d8460857a6c6d50df4
Ryan Cronin <81655542+ryanjcronin5@users.noreply.github.com>
2022-09-06 11:36:56 +1200

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,58 +13,80 @@ class functions():
     def __init__(self):
         pass
 
+    def FHF():
+        col_list= list(functions.df)
+        functions.df['Total'] = functions.df[functions.columnName[1]] + functions.df[functions.columnName[2]] + functions.df[functions.columnName[3]] + functions.df[functions.columnName[4]]
+        functions.columnName = list(functions.df.columns)
 
     def GUI(): # Graphical User Inferface
+        functions.FHF()
         BG = 'gray'
         root = Tk()
         root.title('Monster Card Games')
         root['bg']= BG
         root.geometry("700x400")
-        draw = Canvas(root, bg=BG, bd=0, highlightthickness=0, relief='ridge')
-
+        draw = Canvas(root, width=651, bg=BG, bd=0, highlightthickness=0, relief='ridge')
         MYFONT = "Ubuntu"
 
         # Headers
-
         Label(root, text="Monster Card Data", bg=BG, font=MYFONT).place(x=10,y=10)
         Label(root, text="Click the name to edit and view the card", bg=BG, font=(MYFONT,10)).place(x=190,y=16)
         Label(root, text="File Name: "+functions.FILENAME, bg=BG, font=(MYFONT,10)).place(x=550,y=16)
 
         #Header Row
-
-
-        draw.create_rectangle(0,0,90,30,fill="white")
-        draw.create_rectangle(90,0,180,30,fill="white")
-        draw.create_rectangle(180,0,270,30,fill="white")
-        draw.create_rectangle(270,0,360,30,fill="white")
-        draw.place(x=10,y=50)
+        draw.create_rectangle(0,0,140,30,fill="white")
+        draw.create_rectangle(140,0,245,30,fill="white")
+        draw.create_rectangle(245,0,350,30,fill="white")
+        draw.create_rectangle(350,0,455,30,fill="white")
+        draw.create_rectangle(455,0,560,30,fill="white")
+        draw.create_rectangle(560,0,630,30,fill="white")      
 
         Label(root, text=functions.columnName[0], bg='white', font=MYFONT).place(x=11,y=51)
-        Label(root, text=functions.columnName[1], bg='white', font=MYFONT).place(x=101,y=51)
-        Label(root, text=functions.columnName[2], bg='white', font=MYFONT).place(x=191,y=51)
-        Label(root, text=functions.columnName[3], bg='white', font=MYFONT).place(x=281,y=51)
+        Label(root, text=functions.columnName[1], bg='white', font=MYFONT).place(x=151,y=51)
+        Label(root, text=functions.columnName[2], bg='white', font=MYFONT).place(x=256,y=51)
+        Label(root, text=functions.columnName[3], bg='white', font=MYFONT).place(x=361,y=51)
+        Label(root, text=functions.columnName[4], bg='white', font=MYFONT).place(x=466,y=51)
+        Label(root, text=functions.columnName[5], bg='white', font=MYFONT).place(x=571,y=51)
+        
+        Y1=30
+        Y=51
+        for row in functions.df.index:
+            Y2 = Y1+30
+            Y = Y+30
+            draw.create_rectangle(0,Y1,140,Y2,fill='white')
+            Label(root, text=functions.df[functions.columnName[0]][row], bg='white', font=MYFONT).place(x=11,y=Y)
+            draw.create_rectangle(140,Y1,245,Y2,fill='white')
+            Label(root, text=functions.df[functions.columnName[1]][row], bg='white', font=MYFONT).place(x=151,y=Y)
+            draw.create_rectangle(245,Y1,350,Y2,fill='white')
+            Label(root, text=functions.df[functions.columnName[2]][row], bg='white', font=MYFONT).place(x=256,y=Y)
+            draw.create_rectangle(350,Y1,455,Y2,fill='white')
+            Label(root, text=functions.df[functions.columnName[3]][row], bg='white', font=MYFONT).place(x=361,y=Y)
+            draw.create_rectangle(455,Y1,560,Y2,fill='white')
+            Label(root, text=functions.df[functions.columnName[4]][row], bg='white', font=MYFONT).place(x=466,y=Y)
+            draw.create_rectangle(560,Y1,630,Y2,fill='white')
+            Label(root, text=functions.df[functions.columnName[5]][row], bg='white', font=MYFONT).place(x=571,y=Y)
+            Y1 = Y1+30
+        draw.place(x=10,y=50)
 
+        
+    def UAR(): # User Add Record
+        pass
         
 
 
-    def UAR(): #User Add Record
-        pass
-        
-
-
-    def USR():
+    def USR(): # User Save Record
         pass
 
 
-    def UDR():
+    def UDR(): # User Delete Record
         pass
 
 
-    def UDEV():
+    def UDEV(): # User Data Empty Validation
         pass
 
 
-    def UDIV():
+    def UDIV(): # User Data Invalid Validation
         pass
 
 
