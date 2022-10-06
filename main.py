82c314c10ce36827ced440c7085b047aa55790bf
Ryan Cronin <81655542+ryanjcronin5@users.noreply.github.com>
2022-10-06 15:59:42 +1300

    Updates up to 6/10/2022

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
 from functions import *
-from time import perf_counter
-from datetime import date
-
-MAXCARDS = 9
-MAXPOINTS = 25
-MINPOINTS = 1
-NAMELIMIT = r"^[a-zA-Z0-9]*$"
-eFlag = 0
-iFlag = 0
-day = date.today()
-today = day.strftime("%d - %m - %y")
-
-
 
 if __name__ == "__main__":
-    functions.main()
+    Card.main()
